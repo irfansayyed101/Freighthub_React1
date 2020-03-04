559d7dac552c01b21cd4d6d7a6a5e58c885d00b7
Irfan Sayyed <irfan_sayyed@persistent.co.in>
2020-03-04 19:47:08 +0530

    Shipment Table

--- client/src/containers/TableComponent.js
+++ client/src/containers/TableComponent.js
@@ -4,6 +4,7 @@ import history from '../common/history';
 import PaginationComp from '../components/PaginationComp';
 import SearchShipment from '../components/SearchnameComp';
 import SortTable from '../components/SortTable';
+import ShipmentTable from '../components/ShipmentTable';
 
 /* This is the main functional component which gets exported */
 const TableComponent = (props) => {
@@ -103,44 +104,5 @@ const TableComponent = (props) => {
     </div>
     )
   }
-       
-    /*  Shipment Table component */ 
-  const ShipmentTable = props => (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Mode</th>
-          <th>Type</th>
-          <th>Destination</th>
-          <th>Origin</th>
-          <th>Total</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.shipmement.length > 0 ? (
-          props.shipmement.map((ship,key) => (
-            <tr key={key} onClick={() => {
-              props.editRow(ship)
-          }}>
-              <td>{ship.name}</td>
-              <td>{ship.mode}</td>
-              <td>{ship.type}</td>
-              <td>{ship.destination}</td>
-              <td>{ship.origin}</td>
-              <td>{ship.total}</td>
-              <td>{ship.status}</td>
-              
-            </tr>
-          ))
-        ) : (
-          <tr>
-            <td colSpan={6}>No shipmement data available </td>
-          </tr>
-        )}
-      </tbody>
-    </table>
-  )
 
 export default TableComponent; // Exposes the component to other modules.
